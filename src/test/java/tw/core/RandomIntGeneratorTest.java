06a35e27653ceac9f62efe593394ebbe0b8768b2
Qianru <2583543607@qq.com>
2019-04-11 16:41:26 +0800

    commit

--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -4,6 +4,36 @@ package tw.core;
 /**
  * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
  */
-public class RandomIntGeneratorTest {
+import org.junit.Before;
+import org.junit.Test;
+import tw.core.generator.RandomIntGenerator;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class RandomIntGeneratorTest {
+	private RandomIntGenerator randomIntGenerator;
+
+    @Before
+    public void setUp() throws Exception {
+        this.randomIntGenerator = new RandomIntGenerator();
+    }
+
+    @Test
+    public void should_get_4_digits_numstr_and_every_digit_less_than_10() throws Exception {
+        //given
+        //when
+        String numStr = randomIntGenerator.generateNums(9, 4);
+        //then
+        assertThat(numStr.length(), is(7));
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_throw_exception_when_dufutmax_less_than_numbersOfNeed() throws Exception {
+
+        //given
+        //when
+        String numStr = randomIntGenerator.generateNums(4, 5);
+    }
 }
--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -3,7 +3,39 @@ package tw.core.generator;
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
-public class AnswerGeneratorTest {
+import tw.core.Answer;
+import tw.core.exception.OutOfRangeAnswerException;
+import tw.core.generator.AnswerGenerator;
+import tw.core.generator.RandomIntGenerator;
+import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class AnswerGeneratorTest {
+	@Test(expected = OutOfRangeAnswerException.class)
+    public void should_throw_OutOfRangeAnswerException_which_is_not_between_0_and_9() throws OutOfRangeAnswerException {
+        RandomIntGenerator randomIntGenerator = mock(RandomIntGenerator.class);
+        when(randomIntGenerator.generateNums(anyInt(), anyInt())).thenReturn("1 2 3 10");
+        AnswerGenerator answerGenerator = new AnswerGenerator(randomIntGenerator);
+
+        answerGenerator.generate();
+
+    }
+
+    @Test
+    public void should_get_radam_number() throws Exception {
+        RandomIntGenerator randomIntGenerator = mock(RandomIntGenerator.class);
+        when(randomIntGenerator.generateNums(anyInt(), anyInt())).thenReturn("1 2 3 4");
+        AnswerGenerator answerGenerator = new AnswerGenerator(randomIntGenerator);
+
+        Answer answer = answerGenerator.generate();
+        assertNotNull(answer);
+        assertThat(answer.getIndexOfNum("4"), is(3));
+    }
 }
 
